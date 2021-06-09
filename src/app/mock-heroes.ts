b4c31dbc2b0ed6739a7e876d574a62fad606c903
Sam Baloyi <sambaloyi2017@gmail.com>
2021-06-09 21:11:33 +0200

    Initial tour of heroes commit

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES : Hero[] = [
+  {id: 11, name: "Dr Strange"},
+  {id: 12, name: "Iron Man"},
+  {id: 13, name: "Thor"},
+  {id: 14, name: "Thor"},
+  {id: 15, name: "Hulk"},
+  {id: 16, name: "Ant Man"},
+  {id: 17, name: "Captain America"},
+  {id: 18, name: "Black Panther"},
+  {id: 19, name: "Homelander"},
+  {id: 20, name: "Omni Man"}
+];
